1ce2cc3b6cdce714e8d0a3928fbf536aae247dc4
atmygml <43545518+atmygml@users.noreply.github.com>
2022-04-11 12:15:57 +0800

    Update flutter_check_for_numbers_not_empty_null.dart

--- flutter_check_for_numbers_not_empty_null.dart
+++ flutter_check_for_numbers_not_empty_null.dart
@@ -8,3 +8,20 @@ onPressed: () {
                       txTitle: titleController.text,
                       txAmount: double.parse(amountController.text),
                     );
+                    
+// Check also for positive numbers only
+
+onPressed: () {
+                  if (titleController.text.isNotEmpty &&
+                      amountController.text.isNotEmpty &&
+                      (num.tryParse(amountController.text)) != null &&
+                      (amountController.text != "NaN") &&
+                      (amountController.text != "-NaN") &&
+                      (num.tryParse(amountController.text)! > 0)) {
+                    addNewTx(
+                      txTitle: titleController.text,
+                      txAmount: double.parse(amountController.text),
+                    );
+                  }
+                },                    
+                    
