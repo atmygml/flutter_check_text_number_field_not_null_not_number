cd9fb806bcabcfc3b23a3971f36f201a7d9d92db
atmygml <43545518+atmygml@users.noreply.github.com>
2022-04-11 19:30:46 +0800

    Update flutter_check_for_numbers_not_empty_null.dart

--- flutter_check_for_numbers_not_empty_null.dart
+++ flutter_check_for_numbers_not_empty_null.dart
@@ -1,3 +1,22 @@
+
+if (titleController.text.isEmpty ||
+        amountController.text.isEmpty ||
+        ((num.tryParse(amountController.text)) == null) ||
+        (amountController.text == "NaN") ||
+        (amountController.text == "-NaN") ||
+        (num.tryParse(amountController.text)! <= 0)) {
+      return;
+    }
+
+widget.addNewTx(
+      txTitle: titleController.text,
+      txAmount: double.parse(amountController.text),
+    );
+
+Navigator.of(context).pop();
+
+// Note: Alternative but should use the method above, using the || operators
+
 onPressed: () {
                   if (titleController.text.isNotEmpty &&
                       amountController.text.isNotEmpty &&
